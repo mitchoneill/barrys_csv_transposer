c6889dfb42bcf70990a3f544221355da483c1183
mitch <mitch@repositpower.com>
2016-08-22 20:01:48 +1000

    fixed grammar

--- transpose.py
+++ transpose.py
@@ -25,9 +25,8 @@ with open(CSV_FILE, newline='') as csvfile:
     reader = csv.reader(csvfile)
     have_read_columns = False
 
-# Default dictionaries are great when you don't know if
-# a key exists yet or not, you can just assume the key
-# does exist and the (list) part means we can also
+# Default dictionaries are great when you don't know if a key exists yet or not,
+# you can just assume the key does exist and the (list) part means we can also
 # assume the value will be a list no matter what
 # https://docs.python.org/3/library/collections.html#collections.defaultdict
 
@@ -40,14 +39,17 @@ with open(CSV_FILE, newline='') as csvfile:
 
         # The first row will be the column names, so we need to find out
         # which column the ID and DATE is in. idx is just short for index
+
         if have_read_columns is False:
             id_idx = row.index(ID_HEADING)
             date_idx = row.index(DATE_HEADING)
             have_read_columns = True
 
-        # Now when we read a data row, we want to get the value in the
-        # column of our ID (the row[id_idx]) and make the our key,
-        # then add the date to 
+        # Now when we read a row from the csv file, we want to get the value
+        # in the column of our ID (the row[id_idx]) and make the our key,
+        # then add the date to the ever growing list of dates attached to
+        # that key
+ 
         else:
             result[row[id_idx]].append(row[date_idx])
 
@@ -55,7 +57,7 @@ if DICTIONARY_OUTPUT:
     print(result)
 
 # Now we have to write the output file, our new file will have the same
-# filename as the input file but will have 'processed' at the start of it
+# filename as the input file but will have 'processed-' at the start of it
 # (dont worry the old file doesnt get deleted).
 # For each key (ie. ID) we have we're going to write that to the first column
 # of each row and then its dates to the other columns
